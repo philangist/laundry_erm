f2912b4ac516e0d9ece739e2be055a4e05f21f99
Phil Opaola <phillipsopaolacoding@gmail.com>
2013-07-28 01:52:14 -0400

    added shirts editing functionality

--- launder/views.py
+++ launder/views.py
@@ -2,11 +2,41 @@ from django.shortcuts import get_object_or_404, render_to_response, render
 from django.http import HttpResponseRedirect, HttpResponse
 from  django.core.urlresolvers import reverse
 
-import logger_factory
 from launder.forms import WashFoldOrderForm
 from launder.models import WashFoldOrder, DryCleaning, LaundryShirtsOrder
 
+import logger_factory
+
 logger = logger_factory.logger_factory('views')
+
+def make_wash_fold_instance(request):
+	logger.info('entered make_wash_fold_instance')
+	wash_fold_instance = WashFoldOrder(
+		first_name = request.POST['first_name'],
+		last_name = request.POST['last_name'],
+		phone_number = request.POST['phone_number'],
+		address = request.POST['address'],
+		total_cost = float(request.POST['total_cost']),
+		payment_method = request.POST['payment_method'],
+		payment_finalized = request.POST['payment_finalized']
+	)
+	logger.info('wash_fold_instance created')
+	return wash_fold_instance
+
+def edit_wash_fold_instance(request, wash_fold_id):
+	logger.info('entered edit_wash_fold_instance')
+	wash_fold_instance = WashFoldOrder.objects.get(pk=wash_fold_id)
+	logger.info('wash_fold_instance.__dict__: %s' % str(wash_fold_instance.__dict__))
+	wash_fold_instance.first_name = request.POST['first_name']
+	wash_fold_instance.last_name = request.POST['last_name']
+	wash_fold_instance.phone_number = request.POST['phone_number']
+	wash_fold_instance.address = request.POST['address']
+	wash_fold_instance.total_cost = float(request.POST['total_cost'])
+	wash_fold_instance.payment_method = request.POST['payment_method']
+	wash_fold_instance.payment_finalized = request.POST['payment_finalized']
+	logger.info('wash_fold_instance edited')
+	logger.info('edited wash_fold_instance.__dict__: %s' % str(wash_fold_instance.__dict__))
+	return wash_fold_instance
 
 def make_dry_clean_instance(request):
 	logger.info('entered make_dry_clean_instance')
@@ -24,34 +54,22 @@ def make_dry_clean_instance(request):
 	logger.info('dry_cleaning_instance created: %s' % str(dry_cleaning_instance.__dict__))
 	return dry_cleaning_instance
 
-def edit_wash_fold_instance(request, wash_fold_id):
-	logger.info('entered edit_wash_fold_instance')
-	wash_fold_instance = WashFoldOrder.objects.get(pk=wash_fold_id)
-	logger.info('wash_fold_instance.__dict__: %s' % str(wash_fold_instance.__dict__))
-	wash_fold_instance.first_name = request.POST['first_name']
-	wash_fold_instance.last_name = request.POST['last_name']
-	wash_fold_instance.phone_number = request.POST['phone_number']
-	wash_fold_instance.address = request.POST['address']
-	wash_fold_instance.total_cost = float(request.POST['total_cost'])
-	wash_fold_instance.payment_method = request.POST['payment_method']
-	wash_fold_instance.payment_finalized = request.POST['payment_finalized']
-	logger.info('wash_fold_instance edited')
-	logger.info('edited wash_fold_instance.__dict__: %s' % str(wash_fold_instance.__dict__))
-	return wash_fold_instance
-
-def make_wash_fold_instance(request):
-	logger.info('entered make_wash_fold_instance')
-	wash_fold_instance = WashFoldOrder(
-		first_name = request.POST['first_name'],
-		last_name = request.POST['last_name'],
-		phone_number = request.POST['phone_number'],
-		address = request.POST['address'],
-		total_cost = float(request.POST['total_cost']),
-		payment_method = request.POST['payment_method'],
-		payment_finalized = request.POST['payment_finalized']
-	)
-	logger.info('wash_fold_instance created')
-	return wash_fold_instance
+def edit_dry_clean_instance(request, dry_clean_id):
+	logger.info('entered edit_dry_clean_instance')
+	dry_clean_instance = WashFoldOrder.objects.get(pk=dry_clean_id)
+	logger.info('dry_clean_instance.__dict__: %s' % str(dry_clean_instance.__dict__))
+	dry_clean_instance.first_name = request.POST['first_name']
+	dry_clean_instance.last_name = request.POST['last_name']
+	dry_clean_instance.phone_number = request.POST['phone_number']
+	dry_clean_instance.address = request.POST['address']
+	dry_clean_instance.garment_type = float(request.POST['garment_type'])
+	dry_clean_instance.garment_amount = float(request.POST['garment_amount'])
+	dry_clean_instance.total_cost = float(request.POST['total_cost'])
+	dry_clean_instance.payment_method = request.POST['payment_method']
+	dry_clean_instance.payment_finalized = request.POST['payment_finalized']
+	logger.info('dry_clean_instance edited')
+	logger.info('edited dry_clean_instance.__dict__: %s' % str(dry_clean_instance.__dict__))
+	return dry_clean_instance
 
 def make_shirts_instance(request):
 	logger.info('entered make_shirts_instance')
@@ -69,6 +87,24 @@ def make_shirts_instance(request):
 	)
 	logger.info('shirts_instance created')
 	return shirts_instance
+
+def edit_shirts_instance(request, shirt_id):
+	logger.info('entered edit_shirt_instance')
+	shirt_instance = LaundryShirtsOrder.objects.get(pk=shirt_id)
+	logger.info('shirt_instance.__dict__: %s' % str(shirt_instance.__dict__))
+	shirt_instance.first_name = request.POST['first_name']
+	shirt_instance.last_name = request.POST['last_name']
+	shirt_instance.phone_number = request.POST['phone_number']
+	shirt_instance.address = request.POST['address']
+	shirt_instance.shirts_amount = int(request.POST['shirts_amount'])
+	shirt_instance.shirts_price = float(request.POST['shirts_price'])
+	shirt_instance.starched = bool(request.POST['starched'])
+	shirt_instance.total_cost = float(request.POST['total_cost'])
+	shirt_instance.payment_method = request.POST['payment_method']
+	shirt_instance.payment_finalized = bool(request.POST['payment_finalized'])
+	logger.info('shirt_instance edited')
+	logger.info('edited shirt_instance.__dict__: %s' % str(shirt_instance.__dict__))
+	return shirt_instance
 
 def dry_clean(request):
 	orders = DryCleaning.objects.all()
@@ -119,7 +155,7 @@ def wash_fold_detail(request, wash_fold_order):
 	)
 
 def wash_fold_add_form(request, wash_fold_order=None):
-	if wash_fold_order != None:
+	if wash_fold_order is not None:
 		wash_fold_order = get_object_or_404(WashFoldOrder, pk=wash_fold_order)
 	return render(request, 'wash_fold_form.html', 
 		{'wash_fold_order' : wash_fold_order }
@@ -161,20 +197,26 @@ def shirts_detail(request, shirt_order):
 	)
 
 def shirts_add_form(request, shirt_order=None):
-	if shirt_order != None:
+	if shirt_order is not None:
 		shirt_order = get_object_or_404(LaundryShirtsOrder, pk=shirt_order)
-	return render_to_response('shirts_form.html', 
+	return render(request, 'shirts_form.html', 
 		{'shirt_order' : shirt_order}
 	)
 
-def shirts_add(request):
+def shirts_add(request, shirt_id=None):
 	logger.info("method: %s" % request.method)
 	if request.method == 'POST':
 		logger.info("POST params: %s" % str(request.POST))
 		try:
 			logger.info('entered try clause')
-			shirt_instance = make_shirts_instance(request)
-			shirt_instance.save()
+			if shirt_id is not None:
+				logger.info('updating shirt')
+				logger.info('shirt_id = %d, type = %s' % (int(shirt_id), str(type(shirt_id))))
+				shirt = edit_shirts_instance(request, shirt_id)
+				logger.info('shirts edited')
+			else:
+				shirt = make_shirts_instance(request)
+			shirt.save()
 			logger.info("New LaundryShirtsOrder instance saved")
 			return HttpResponseRedirect(reverse(shirts))
 		except:
--- laundry/urls.py
+++ laundry/urls.py
@@ -9,21 +9,26 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 	url(r'^admin/', include(admin.site.urls)),
-	url(r'^dry_clean_form/$', 'launder.views.dry_clean_add_form'),
-	url(r'^dry_clean_form/(?P<dry_clean_order>\d+)/$', 'launder.views.dry_clean_add_form'),
-    url(r'^dry_clean/add/$', 'launder.views.dry_clean_add'),
-    url(r'^dry_clean/$', 'launder.views.dry_clean'),
-	url(r'^dry_clean/(?P<dry_clean_order>\d+)/$', 'launder.views.dry_clean_detail'),
+	#wash_fold urls
 	url(r'^wash_fold_form/$', 'launder.views.wash_fold_add_form'),
 	url(r'^wash_fold_form/(?P<wash_fold_order>\d+)/$', 'launder.views.wash_fold_add_form'),
 	url(r'^wash_fold/add/(?P<wash_fold_id>\d+)/$', 'launder.views.wash_fold_add'),
 	url(r'^wash_fold/add/$', 'launder.views.wash_fold_add'),
 	url(r'^wash_fold/$', 'launder.views.wash_fold'),
 	url(r'^wash_fold/(?P<wash_fold_order>\d+)/$', 'launder.views.wash_fold_detail'),
+	#dry_clean urls
+	url(r'^dry_clean_form/$', 'launder.views.dry_clean_add_form'),
+	url(r'^dry_clean_form/(?P<dry_clean_order>\d+)/$', 'launder.views.dry_clean_add_form'),
+    url(r'^dry_clean/add/$', 'launder.views.dry_clean_add'),
+    url(r'^dry_clean/$', 'launder.views.dry_clean'),
+	url(r'^dry_clean/(?P<dry_clean_order>\d+)/$', 'launder.views.dry_clean_detail'),
+	#shirts urls
 	url(r'^shirts_form/$', 'launder.views.shirts_add_form'),
 	url(r'^shirts_form/(?P<shirt_order>\d+)/$', 'launder.views.shirts_add_form'),
+	url(r'^shirts/add/(?P<shirt_id>\d+)/$', 'launder.views.shirts_add'),
+	url(r'^shirts/add/$', 'launder.views.shirts_add'),
     url(r'^shirts/$', 'launder.views.shirts'),
 	url(r'^shirts/(?P<shirt_order>\d+)/$', 'launder.views.shirts_detail'),
-	url(r'^shirts/add/$', 'launder.views.shirts_add'),
+	#index
 	url(r'^$', 'launder.views.index')
 )
